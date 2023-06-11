543e6bae8a908a437b81eb50d7b93b0cc213f48f
plaha303 <111688671+plaha303@users.noreply.github.com>
2023-06-11 10:12:02 +0300

    fix del_empty_folders funk, relocate del_empty_folder and unpack_archives func in main func body

--- main.py
+++ main.py
@@ -5,20 +5,17 @@ import shutil
 
 from normalize import normalize
 
-CATEGORIES = {"IMAGE": ['.jpeg', '.png', '.jpg', '.svg'],
-              "VIDEO": ['.avi', '.mp4', '.mov', '.mkv'],
-              "DOCS": ['.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx'],
-              "MUSIC": ['.mp3', '.ogg', '.wav', '.amr'],
-              "ARCHIVE": ['.zip', '.gz', '.tar']}
+CATEGORIES = {"Pictures": ['.jpeg', '.png', '.jpg', '.svg'],
+              "Video": ['.avi', '.mp4', '.mov', '.mkv'],
+              "Documents": ['.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx'],
+              "Music": ['.mp3', '.ogg', '.wav', '.amr'],
+              "Archive": ['.zip', '.gz', '.tar']}
 
 
-def move_file(file: Path, root_dir: Path, categorie: str) -> None:
-    target_dir = root_dir.joinpath(categorie)
+def move_file(file: Path, root_dir: Path, categories: str) -> None:
+    target_dir = root_dir.joinpath(categories)
     if not target_dir.exists():
-        # print(f"Make {target_dir}")
         target_dir.mkdir()
-    # print(path.suffix)
-    # print(target_dir.joinpath(f"{normalize(path.stem)}{path.suffix}"))
     new_name = target_dir.joinpath(f"{normalize(file.stem)}{file.suffix}")
     if new_name.exists():
         new_name = new_name.with_name(f"{new_name.stem}_{uuid.uuid4()}{file.suffix}")
@@ -27,34 +24,36 @@ def move_file(file: Path, root_dir: Path, categorie: str) -> None:
 
 def get_categories(file: Path) -> str:
     ext = file.suffix.lower()
-    for cat, exts in CATEGORIES.items():
-        if ext in exts:
+    for cat, exist in CATEGORIES.items():
+        if ext in exist:
             return cat
     return "Other"
 
 
 def sort_folder(path: Path) -> None:
     for item in path.glob("**/*"):
-        # print(item)
         if item.is_file():
             cat = get_categories(item)
             move_file(item, path, cat)
 
-    delete_empty_folders(path)
-    unpack_archives(path)
+    # delete_empty_folders(path)
+    # unpack_archives(path)
 
 
 def delete_empty_folders(path: Path):
-    for item in path.glob("**/*"):
-        if item.is_dir() and not any(item.iterdir()):
-            item.rmdir()
+    try:
+        for item in list(path.glob("**/*"))[::-1]:
+            if item.is_dir() and item.name not in CATEGORIES:
+                item.rmdir()
+    except OSError as e:
+        return print(f'Error {e}')
 
 
 def unpack_archives(path: Path) -> None:
-    archive_dir = path.joinpath("ARCHIVE")
+    archive_dir = path.joinpath("Archive")
     archive_dir.mkdir(exist_ok=True)
     for item in path.glob("**/*"):
-        if item.is_file() and item.suffix.lower() in CATEGORIES["ARCHIVE"]:
+        if item.is_file() and item.suffix.lower() in CATEGORIES["Archive"]:
             target_dir = archive_dir.joinpath(item.stem)
             target_dir.mkdir(parents=True, exist_ok=True)
             shutil.unpack_archive(str(item), str(target_dir))
@@ -70,8 +69,8 @@ def main():
         return f"Folder with path {path} dos`n exists."
 
     sort_folder(path)
-    # delete_empty_folders(path)
-    # upack_archive(path)
+    delete_empty_folders(path)
+    unpack_archives(path)
 
     return "All ok"
 
